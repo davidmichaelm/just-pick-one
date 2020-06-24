4245b4ad81b83dc75c37c899a0bd138bbbfc7229
David Marquardt <davemarquardt@gmail.com>
2020-06-24 10:21:40 -0500

    Added more results data

--- src/components/Result.svelte
+++ src/components/Result.svelte
@@ -1,9 +1,20 @@
 <script>
     export let data;
+
+    let directionsUrl = "https://www.google.com/maps/dir/?api=1&destination=" + encodeStringForUrl(data.name);
+
+    function encodeStringForUrl(string) {
+        return string.replace(/ /gi, '+');
+    }
 </script>
 
 <div>
     <img src="{data.image_url}" height="100" width="100">
     <a href="{data.url}">{data.name}</a>
-    <a href="">Directions</a>
+    <span>Rating: {data.rating}</span>
+    <span>Price: {data.price}</span>
+    <a href="{directionsUrl}">Directions</a>
+    {#each data.transactions as transaction}
+        <span>{transaction}</span>
+    {/each}
 </div>
